ef81b0350c8eb8bba992ff3a8851780dcbb8396e
DenisPeleng <rangerby@icloud.com>
2022-09-24 12:06:16 +0400

    Initial commit

---
+++ ASCII Mirror/task/src/asciimirror/Main.java
@@ -0,0 +1,18 @@
+package asciimirror;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Input the file path:");
+        Scanner scanner = new Scanner(System.in);
+        String filePath = scanner.nextLine();
+        System.out.println(filePath);
+        System.out.println("            ^__^");
+        System.out.println("    _______/(oo)");
+        System.out.println("/\\/(       /(__)");
+        System.out.println("   | w----||    ");
+        System.out.println("   ||     ||    ");
+    }
+}
